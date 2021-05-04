cd538df5542c3ba4416074cb86e59d4f1104ab4e
Anshul <samgalacestyle01@gmail.com>
2021-05-04 11:33:48 +0530

    Implemented the api code to create and view posts

--- zappit_project/posts/views.py
+++ zappit_project/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
+        
