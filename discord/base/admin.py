dea42866c14479b91a3039b8123898eebbe9e302
Anshul Jain <samgalacestyle01@gmail.com>
2022-01-08 23:11:03 +0530

    Implemented CRUD operations on discord

--- discord/base/admin.py
+++ discord/base/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room
+# import the room table/model
+from .models import Room, Topic, Message
 
+# register the room table/model
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
